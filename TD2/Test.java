2d9cc5bc022d638be8eb859a8ee421128372bfe2
William <william.fijean@gmail.com>
2025-02-07 14:19:59 +0000

    temp.java

--- TD2/Test.java
+++ TD2/Test.java
@@ -1,9 +1,6 @@
-package TD2;
+
 public class Test {
     public static void main(String[] args) {
-        Confiture c1 = new Confiture("Fraise",50,120);
-        Confiture c2 = new Confiture("Fraise",50,120);
-        System.out.println(c1.egal(c2));
-        System.out.println(c1==c2);
+        Temperature temp1 = new Temperature(120, "dezez");
     }
 }
